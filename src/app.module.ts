6c0079d25788df20323bc2eee1052724705be132
Istvan Petrovan <petrovanistvan@gmail.com>
2023-03-28 16:01:21 +0200

    added basic auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
 })
 export class AppModule {}
